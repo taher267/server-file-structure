18367f92cfcac776f9fe600a6e007ff5190f2f56
ABU TAHER <abutaher267@gmail.com>
2023-09-15 19:36:46 +0600

    remoe express rate limit

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,12 +1,12 @@
-const express = require('express'),
-  { rateLimit } = require('express-rate-limit');
+const express = require('express');
+// { rateLimit } = require('express-rate-limit');
 const middlewires = [
   express.json(),
   require('morgan')('dev'),
-  rateLimit({
-    windowMs: 10 * 60 * 1000,
-    limit: 100,
-  }),
+  // rateLimit({
+  //   windowMs: 10 * 60 * 1000,
+  //   limit: 100,
+  // }),
 ];
 module.exports = (app) => {
   app.use(middlewires);
